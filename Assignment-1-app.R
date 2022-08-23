357a0230de9e69cbd936c5a0a32aab594163ba6b
Harsh Katiyar <hkat0008@student.monash.edu>
2022-08-23 17:16:04 +1000

    Modified the app and added the link of the app after publishing it

--- Assignment-1-app.R
+++ Assignment-1-app.R
@@ -1,18 +1,23 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(shinythemes)
 
 
-mydata1<- read_csv(url("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-29/state_milk_production.csv"))
+mydata1 <- read_csv(url("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-29/state_milk_production.csv"))
 
 mydata2 <- read_csv(url("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-29/milk_products_facts.csv")) %>%
   pivot_longer(cols = fluid_milk:dry_whey,
                names_to = "Product_Type",
                values_to = "Consumption")
 
+mydata3 <- read_csv(url("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-29/fluid_milk_sales.csv"))
+
 
 ui <- fluidPage(
-  titlePanel("Dairy Production and Consumption Statistics"),
+  theme = shinytheme("darkly"),
+
+  titlePanel("DAIRY PRODUCTION AND CONSUMPTION STATISTICS"),
   h3("1. What is the average production of milk per capita in each state of United States from 1970 to 2017"),
   fluidRow(
         radioButtons("productionRegion",
@@ -21,17 +26,31 @@ ui <- fluidPage(
         selectInput("production",
                     "Select State:",
                     choices = unique(mydata1$state)),
+
         mainPanel(
           plotlyOutput("plot1")
+
         )
   ),
-  h3("2. What is consumption of different dairy products per capita from 1975 to 2017"),
+
+  h3("2. What is the direct sales/consumption of different categories of fluid milk as compared total milk production per year?"),
+  fluidRow(
+    selectInput("sales",
+                "Select Category",
+                choices = unique(mydata3$milk_type)),
+    mainPanel(
+      plotlyOutput("plot2"),
+      plotlyOutput("totalPlot")
+    )
+  ),
+
+  h3("3. What is consumption of different dairy products per capita per person from 1975 to 2017"),
   fluidRow(
     selectInput("consumption",
                 "Select Product:",
                 choices = unique(mydata2$Product_Type)),
     mainPanel(
-      plotlyOutput("plot2")
+      plotlyOutput("plot3")
     )
   ),
   includeCSS("styles.css")
@@ -62,10 +81,53 @@ server <- function(input, output, session) {
   })
 
   output$plot2 <- renderPlotly({
+    sales_plot <- mydata3 %>%
+      filter(milk_type == input$sales)
+
+    p1 <- ggplot(sales_plot, aes(year, pounds/1000000)) +
+      geom_line(stat = 'identity') +
+      theme_bw(base_size = 14) +
+      labs(x = "Year", y = "Sales/Consumption (million lbs)") +
+      scale_x_continuous(
+        breaks = seq(min(sales_plot$year),
+                     max(sales_plot$year), by=1)) +
+      theme(
+        axis.text.x = element_text(angle = 90,
+                                   hjust = 1))
+
+    ggplotly(p1)
+
+
+  })
+
+    output$totalPlot <- renderPlotly({
+      total_production <- mydata1 %>%
+        group_by(year) %>%
+        summarise(Total_Production = sum(milk_produced))
+
+    p2 <- ggplot(total_production, aes(year, Total_Production/1000000)) +
+      geom_line(stat = 'identity') +
+      theme_bw(base_size = 14) +
+      labs(x = "Year", y = "Total Production (million lbs)") +
+      scale_x_continuous(
+        breaks = seq(min(total_production$year),
+                     max(total_production$year), by=1)) +
+      theme(
+        axis.text.x = element_text(angle = 90,
+                                   hjust = 1))
+
+    ggplotly(p2)
+
+
+
+  })
+
+
+  output$plot3 <- renderPlotly({
     consumption_plot <- mydata2 %>%
       filter(Product_Type == input$consumption)
 
-    p1 <- ggplot(consumption_plot, aes(year, Consumption)) +
+    p3 <- ggplot(consumption_plot, aes(year, Consumption)) +
       geom_col(stat = 'identity') +
       theme_bw(base_size = 14) +
       labs(x = "Year", y = "Consumption (lbs per person)") +
@@ -76,7 +138,7 @@ server <- function(input, output, session) {
         axis.text.x = element_text(angle = 90,
                                    hjust = 1))
 
-    ggplotly(p1)
+    ggplotly(p3)
   })
 
 }
--- README.md
+++ README.md
@@ -6,8 +6,10 @@ This is the **Dairy production and consumption** app for the ETC5523 Assessment 
 * The creator of this app is `Harsh Katiyar`.
 * The data used for this assessment is the `Dairy Production and Consumption Data` data. 
 
-The goal of the app is to study and visualize the statistics of MILK PRODUCTION from 1970-2017 to explain the trend of milk production in different regions of USA and their states. Apart from this, in the other part of app, the purpose is to study the statistics of each dairy product consumption per person from 1975 to 2017 with better visualization. 
+The goal of the app is to study and visualize the statistics of MILK PRODUCTION from 1970-2017 to explain the trend of milk production in different regions of USA and their states. The app will also show the total milk production per year.
 
-From the above two statistical visualizations, one can also find out the relationship between production and consumption of milk products.
+In the second part of the app, a column graph is used to show the total fluid milk sales/consumption per year and in the last part of the app, the purpose is to study the statistics of each dairy product consumption per person from 1975 to 2017 with better visualization. 
 
-The link to the app can be found at 
+From the above statistical and mathematical calculation, one can find out the relationship between Milk production, fluid milk sales/consumption and dairy products cosumption.
+
+The link to the app can be found at [https://nanqfu-harsh-katiyar.shinyapps.io/shiny--Harshk1202/](https://nanqfu-harsh-katiyar.shinyapps.io/shiny--Harshk1202/)
